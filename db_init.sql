01973dd29c0d6e8fb3afbf3a612959efccbe83c1
Lorenzo Soto <lorenzo.soto@gmail.com>
2022-04-30 02:20:32 +0200

    populate db with more users/streams

--- db_init.sql
+++ db_init.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS streams;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -14,21 +15,29 @@ CREATE TABLE streams (
 	id serial PRIMARY KEY,
 	name VARCHAR ( 100 ) NOT NULL,
 	user_id INT NOT NULL,
-	CONSTRAINT fk_user
-		FOREIGN KEY(user_id) 
-		REFERENCES users(id)
-		ON DELETE CASCADE
+	FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 INSERT INTO 
     users (name, email, password, created_on)
 VALUES
-    ('Guest', 'guest@tocata.com', '$2b$10$zHL3BTBPwVLx.Y8PK2aIuub4CVrEZwqcJu8KzJZHItOxOROZOCaVS', current_timestamp);
+    ('User1', 'user1@tocata.com', '$2b$10$zHL3BTBPwVLx.Y8PK2aIuub4CVrEZwqcJu8KzJZHItOxOROZOCaVS', current_timestamp),
+    ('User2', 'user2@tocata.com', '$2b$10$zHL3BTBPwVLx.Y8PK2aIuub4CVrEZwqcJu8KzJZHItOxOROZOCaVS', current_timestamp),
+    ('User3', 'user3@tocata.com', '$2b$10$zHL3BTBPwVLx.Y8PK2aIuub4CVrEZwqcJu8KzJZHItOxOROZOCaVS', current_timestamp),
+    ('User4', 'user4@tocata.com', '$2b$10$zHL3BTBPwVLx.Y8PK2aIuub4CVrEZwqcJu8KzJZHItOxOROZOCaVS', current_timestamp);
 
 INSERT INTO 
     streams (name, user_id)
 VALUES
-    ('Guest Stream 1', 1),
-    ('Guest Stream 2', 1),
-    ('Guest Stream 3', 1),
-    ('Guest Stream 4', 1);
+    ('Stream1', 1),
+    ('Stream2', 1),
+    ('Stream3', 1),
+    ('Stream1', 2),
+    ('Stream2', 2),
+    ('Stream3', 2),
+    ('Stream1', 3),
+    ('Stream2', 3),
+    ('Stream3', 3),
+    ('Stream1', 4),
+    ('Stream2', 4),
+    ('Stream3', 4);
